de384e2381b695fb9c82ae793059351cae53d5c8
GERazZzaR <julian@scheinerj.de>
2021-04-09 13:20:32 +0200

    node version

--- vue.config.js
+++ vue.config.js
@@ -24,14 +24,5 @@ module.exports = {
         chunks: 'all'
       }
     }
-  },
-  chainWebpack: config => {
-    config.plugin('VuetifyLoaderPlugin').tap(args => [{
-      match (originalTag, { kebabTag, camelTag, path, component }) {
-        if (kebabTag.startsWith('core-')) {
-          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
-        }
-      }
-    }])
   }
 }
