4fe0cad3a03a184cd1bd01c6efd4a3e3582a51cf
GERazZzaR <julian@scheinerj.de>
2021-04-12 16:58:54 +0200

    mapkey

--- src/plugins/vue-google-maps.js
+++ src/plugins/vue-google-maps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.use(VueGoogleMaps, {
     load: {
-        key: 'AIzaSyAvw2-EXN9KDOly2VBWkctz1T6izuYXC2k'
+        key: process.env.googlemapskey
     },
 })
 export { VueGoogleMaps };
