9b91fc443e242889e6893e5ff8a246db199d40f9
GERazZzaR <julian@scheinerj.de>
2021-05-24 21:31:31 +0200

    redirect Https

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 app = express();
+app.use(requireHTTPS);
 app.use(serveStatic(path.join(__dirname, 'dist')));
+
 const port = process.env.PORT || 80;
 app.listen(port);
